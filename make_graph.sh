f4d9925efc83c20fbc9da07321171973f5e54e85
yukitomo <over.the.tr0ouble@gmail.com>
2015-03-09 14:40:41 +0900

    test14to27

--- make_graph.sh
+++ make_graph.sh
@@ -1,17 +1,27 @@
-#ROC
+
+python ../evaluation_scripts/plot_curves.py PR  pr_curves_baseline_from14to27_r001.png  ../baseline_new/pr_curve_from25to27_baseline.txt ../proposed_new/pr_curve_from14to27_r001.txt
+
+python ../evaluation_scripts/plot_curves.py ROC tprfpr_curves_baseline_from14to27_r001.png ../baseline_new/tprfpr_curve_from25to27_baseline.txt ../proposed_new/tprfpr_curve_from14to27_r001.txt
+
+python ../evaluation_scripts/plot_curves.py PR  pr_curves_baseline_from14to27_r01.png  ../baseline_new/pr_curve_from25to27_baseline.txt ../proposed_new/pr_curve_from14to27_r01.txt
+
+python ../evaluation_scripts/plot_curves.py ROC tprfpr_curves_baseline_from14to27_r01.png ../baseline_new/tprfpr_curve_from25to27_baseline.txt ../proposed_new/tprfpr_curve_from14to27_r01.txt
+
+#python ../evaluation_scripts/plot_curves.py PR  pr_curves_baseline_from14to27_r02.png  ../baseline_new/pr_curve_from25to27_baseline.txt ../proposed_new/pr_curve_from14to27_r02.txt 
+
+#python ../evaluation_scripts/plot_curves.py ROC tprfpr_curves_baseline_from14to27_r02.png ../baseline_new/tprfpr_curve_from25to27_baseline.txt ../proposed_new/tprfpr_curve_from14to27_r02.txt 
+
+
+
+
 #python ../evaluation_scripts/plot_curves.py ROC tprfpr_curves_baseline_from21to27.png ../baseline_new/tprfpr_curve_from25to27_baseline.txt ../proposed_new/tprfpr_curve_from21to27_r001.txt ../proposed_new/tprfpr_curve_from21to27_r01.txt ../proposed_new/tprfpr_curve_from21to27_r02.txt
-
-python ../evaluation_scripts/plot_curves.py ROC tprfpr_curves_baseline_from21to27_r001.png ../baseline_new/tprfpr_curve_from25to27_baseline.txt ../proposed_new/tprfpr_curve_from21to27_r001.txt 
-
-python ../evaluation_scripts/plot_curves.py ROC tprfpr_curves_baseline_from21to27_r01.png ../baseline_new/tprfpr_curve_from25to27_baseline.txt ../proposed_new/tprfpr_curve_from21to27_r01.txt 
-
-python ../evaluation_scripts/plot_curves.py ROC tprfpr_curves_baseline_from21to27_r02.png ../baseline_new/tprfpr_curve_from25to27_baseline.txt ../proposed_new/tprfpr_curve_from21to27_r02.txt 
-
 
 #python ../evaluation_scripts/plot_curves.py PR  pr_curves_baseline_from21to27.png  ../baseline_new/pr_curve_from25to27_baseline.txt ../proposed_new/pr_curve_from21to27_r001.txt ../proposed_new/pr_curve_from21to27_r01.txt ../proposed_new/pr_curve_from21to27_r02.txt
 
-python ../evaluation_scripts/plot_curves.py PR  pr_curves_baseline_from21to27_r001.png  ../baseline_new/pr_curve_from25to27_baseline.txt ../proposed_new/pr_curve_from21to27_r001.txt
 
-python ../evaluation_scripts/plot_curves.py PR  pr_curves_baseline_from21to27_r01.png  ../baseline_new/pr_curve_from25to27_baseline.txt ../proposed_new/pr_curve_from21to27_r01.txt
 
-python ../evaluation_scripts/plot_curves.py PR  pr_curves_baseline_from21to27_r02.png  ../baseline_new/pr_curve_from25to27_baseline.txt ../proposed_new/pr_curve_from21to27_r02.txt
+python ../evaluation_scripts/plot_curves.py ROC tprfpr_curves_baseline_from14to27.png ../baseline_new/tprfpr_curve_from25to27_baseline.txt ../proposed_new/tprfpr_curve_from14to27_r001.txt ../proposed_new/tprfpr_curve_from14to27_r01.txt 
+
+python ../evaluation_scripts/plot_curves.py PR  pr_curves_baseline_from14to27.png  ../baseline_new/pr_curve_from25to27_baseline.txt ../proposed_new/pr_curve_from14to27_r001.txt ../proposed_new/pr_curve_from14to27_r01.txt 
+
+
