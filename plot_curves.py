ddf334a00d7dfd69f7303d1b34c77b7f08a5d157
yukitomo <over.the.tr0ouble@gmail.com>
2015-03-09 12:07:48 +0900

    modify name result

--- plot_curves.py
+++ plot_curves.py
@@ -7,10 +7,11 @@ input : yx_lists (3 or more)
 output : curve.png
 """
 import sys
-
+import re
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
+import os
 
 def load_submission(file_path):
 	y_elements, x_elements = [], []
@@ -28,17 +29,19 @@ def main():
 	output_file = sys.argv[1]
 	test_results = sys.argv[2:] #複数のプロット用のx,yが格納されたファイル
 	results_lists = []
+	curve_names = []
 
 	for test_result in test_results:
+		directory, curve_name = os.path.split(test_result) #file_name の抽出
+		curve_names.append(curve_name)
 		y_elements, x_elements = load_submission(test_result)
 		plt.plot(x_elements, y_elements)
 
 	plt.xlim(0, 1)
 	plt.ylim(0, 1)
-	plt.legend(test_results,'upper right')
+	plt.legend(curve_names,'upper right')
 	#plt.show()
 	plt.savefig(output_file)	
-
 
 if __name__ == '__main__':
 	main()
